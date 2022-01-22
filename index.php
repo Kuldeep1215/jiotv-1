e72e902a25fa45f2280fbde3a4d112d04bc05f23
Kuldeep1215 <98176167+Kuldeep1215@users.noreply.github.com>
2022-01-22 19:18:17 +0530

    Update index.php
    Localhost:8080 on mobile

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ Need help let me know..  <br>
   To watch a channel on localhost you can watch channels directly in HD and SD <br>
   
 
-<a href="http://localhost/hd.php?c=AXN_HD"target="_blank">AXN HD high<a> | 
-<a href="http://localhost/sd.php?c=AXN_HD"target="_blank">AXN HD low<a> | <br>
+<a href="http://localhost:8080/hd.php?c=AXN_HD"target="_blank">AXN HD high<a> | 
+<a href="http://localhost:8080/sd.php?c=AXN_HD"target="_blank">AXN HD low<a> | <br>
 
 You can change localhost to IP / Domain of your server if you want to watch on VPS <br> 
 Regards 
